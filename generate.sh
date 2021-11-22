0b2b4db04ab899eb5416b50ae3c0b27093d09eba
James Williams <jamesphwilliams@gmail.com>
2021-11-22 23:30:18 +0000

    Generate index.html over result.html

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 ls -1 Google-Sticker-Mashup-Research/stickers/ | grep -v '-' | cut -d. -f1 | tr _ '\n' | sort | uniq > emojis.txt
 
-./generate_table.py > result.html
+./generate_table.py > index.html
